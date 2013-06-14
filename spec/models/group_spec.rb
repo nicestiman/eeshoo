946282a163553ffdf16f0b5e94f7860aa33ca12e
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-14 14:37:41 -0600

    basic group model tests built

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @group = Group.new(name: "Noodle Club", location: "3500 S Wadsworth Blvd, Lakewood, Colorado, 80235") }
+
+  subject { @group }
+
+  it { should respond_to(:name)     }
+  it { should respond_to(:location) }
 end
