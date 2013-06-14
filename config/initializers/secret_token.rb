54363c29b8cb5ee130796ad926678bd5cfe76171
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-14 10:17:55 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HIWIPI::Application.config.secret_token = 'bd73299a42619a4f1575d0188d6c52f32f6741572550164d501d7083743160262c10fb441784f202aa3e49318bb90d845613a7f3ab847b13faf11ce90927e7fc'
