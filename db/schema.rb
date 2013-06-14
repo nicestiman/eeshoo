8d2e5080ae70a6ee797e9f7a7a9e3d4ae881ec49
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-14 14:31:10 -0600

    initialized group model, controller, and environment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130614174134) do
+ActiveRecord::Schema.define(:version => 20130614203007) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "content"
