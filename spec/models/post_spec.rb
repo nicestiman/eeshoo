68ee66a9d061baa42ab6998f4874a111a691494f
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-14 11:58:32 -0600

    database tests pass

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,5 +12,20 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @post = Post.new(content: "This is a test post", title: "Test") }
+
+  subject { @post }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:title)   }
+
+  describe "when content is not present" do
+    before { @post.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when title is not present" do
+    before { @post.title = " " }
+    it { should_not be_valid }
+  end
 end
