b747c806d975b4d8490ef7a31ad738ce4f6b7260
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-17 16:39:41 -0600

    added relations and conditions for tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  location   :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Group < ActiveRecord::Base
   attr_accessible :location, :name
   has_many :posts
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,12 @@
 #  title      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  group_id   :integer
 #
 
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
-  belongs to :group
+  belongs_to :group
 
   validates :content, presence: true
   validates :title,   presence: true
