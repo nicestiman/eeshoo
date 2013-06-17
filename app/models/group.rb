823dbbc0b1a55668c6bc050f60b4123d096be1f9
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-17 15:03:21 -0600

    added length validation to group name

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   attr_accessible :location, :name
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
 
   validates :location, presence: true
 end
