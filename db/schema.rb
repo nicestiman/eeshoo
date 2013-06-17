f07936afd4d19c1dd124e260e7320d7855ade43b
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-17 16:23:35 -0600

    database prepared for new relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130614203007) do
+ActiveRecord::Schema.define(:version => 20130617220149) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(:version => 20130614203007) do
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "group_id"
   end
+
+  add_index "posts", ["group_id"], :name => "index_posts_on_group_id"
 
 end
