7167ca53b127363518654ac59fea8fd3994e1b54
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-18 15:03:21 -0600

    corrected error in index, test is updated

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -1,28 +1,35 @@
 require 'spec_helper'
 
 describe "Post pages" do
+  before do
+    @group = Group.create(name: "Test Group", location: "Los Angeles, California, USA")
+    @post = @group.create(content: "This is a test post", title: "Test")
+  end
 
   subject { page }
 
   describe "new post page" do
-    before { visit new_post_path + ".json" }
 
-    it { should have_content("New Post") }
   end
 
   describe "index" do
     before do
-      Post.create(content: "This is test post #1", title: "Post 1")
-      Post.create(content: "This is test post #2", title: "Post 2")
+      @post1 = Post.create(content: "This is test post #1", title: "Post 1")
+      @post2 = Post.create(content: "This is test post #2", title: "Post 2")
       visit posts_path + ".json"
     end
 
     it "should list each post" do
-      Post.all.each do |post|
-        page.should have_content("post_id: #{post.id}"         )
-        page.should have_content("title: #{post.title}"    )
-        page.should have_content("content: #{post.content}")
-      end
+      #for post 1
+      it { should have_content("post_id:  #{@post1.id}"      ) }
+      it { should have_content("title:    #{@post1.title}"   ) }
+      it { should have_content("content:  #{@post1.content}" ) }
+
+
+      #for post 2
+      it { should have_content("post_id:  #{@post2.id}"      ) }
+      it { should have_content("title:    #{@post2.title}"   ) }
+      it { should have_content("content:  #{@post2.content}" ) }
     end
   end
 end
