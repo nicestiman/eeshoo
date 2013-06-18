d4ab162d332f1cb9c1bbd673e92f6d0492d1a0ad
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-18 15:34:06 -0600

    updated test for new post and improved test for index

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -3,31 +3,55 @@ require 'spec_helper'
 describe "Post pages" do
   before do
     @group = Group.create(name: "Test Group", location: "Los Angeles, California, USA")
-    @post = @group.posts.create(content: "This is a test post", title: "Test")
+    @post = @group.posts.new(content: "This is a test post", title: "Test")
   end
 
   subject { page }
 
   describe "new post page" do
+    before { visit new_post_path }
 
+    let(:submit) { "Post" }
+
+    describe "with invalid information" do
+      it "should not make a new post" do
+        expect { click_button submit }.not_to change(Post, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_content("error") }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Title",    with: @post.title
+        fill_in "Content",  with: @post.content
+      end
+
+      it "should make a new post" do
+        expect { click_button submit }.to change(Post, :count).by(1)
+      end
+    end
   end
 
   describe "index" do
     before do
       @post1 = @group.posts.create(content: "This is test post #1", title: "Post 1")
       @post2 = @group.posts.create(content: "This is test post #2", title: "Post 2")
+      @posts = [@post1, @post2]
       visit posts_path + ".json"
     end
 
-      #for post 1
-      it { should have_content("post_id: #{@post1.id}"      ) }
-      it { should have_content("title: #{@post1.title}"     ) }
-      it { should have_content("content: #{@post1.content}" ) }
-
-
-      #for post 2
-      it { should have_content("post_id: #{@post2.id}"      ) }
-      it { should have_content("title: #{@post2.title}"     ) }
-      it { should have_content("content: #{@post2.content}" ) }
+    it "should list all posts" do
+      @posts.each do |post|
+        #for post 2
+        page.should have_content("post_id: #{post.id}"      )
+        page.should have_content("title: #{post.title}"     )
+        page.should have_content("content: #{post.content}" )
+      end
+    end
   end
 end
