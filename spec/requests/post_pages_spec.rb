de7dada3b44ff573a413264b5831ecce91c286f0
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-18 15:07:24 -0600

    tests corrected using group method

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Post pages" do
   before do
     @group = Group.create(name: "Test Group", location: "Los Angeles, California, USA")
-    @post = @group.create(content: "This is a test post", title: "Test")
+    @post = @group.posts.create(content: "This is a test post", title: "Test")
   end
 
   subject { page }
@@ -14,22 +14,20 @@ describe "Post pages" do
 
   describe "index" do
     before do
-      @post1 = Post.create(content: "This is test post #1", title: "Post 1")
-      @post2 = Post.create(content: "This is test post #2", title: "Post 2")
+      @post1 = @group.posts.create(content: "This is test post #1", title: "Post 1")
+      @post2 = @group.posts.create(content: "This is test post #2", title: "Post 2")
       visit posts_path + ".json"
     end
 
-    it "should list each post" do
       #for post 1
-      it { should have_content("post_id:  #{@post1.id}"      ) }
-      it { should have_content("title:    #{@post1.title}"   ) }
-      it { should have_content("content:  #{@post1.content}" ) }
+      it { should have_content("post_id: #{@post1.id}"      ) }
+      it { should have_content("title: #{@post1.title}"     ) }
+      it { should have_content("content: #{@post1.content}" ) }
 
 
       #for post 2
-      it { should have_content("post_id:  #{@post2.id}"      ) }
-      it { should have_content("title:    #{@post2.title}"   ) }
-      it { should have_content("content:  #{@post2.content}" ) }
-    end
+      it { should have_content("post_id: #{@post2.id}"      ) }
+      it { should have_content("title: #{@post2.title}"     ) }
+      it { should have_content("content: #{@post2.content}" ) }
   end
 end
