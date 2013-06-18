deb35d3c29e6fe75e60c5d05ff346d0c7e07afe8
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-18 11:12:17 -0600

    added capability for valid information

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,9 +13,12 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(params[:group])
     if @group.save
-
+      redirect_to @group
     else
       render 'new'
     end
+  end
+
+  def show
   end
 end
