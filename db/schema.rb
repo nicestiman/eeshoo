aaf03c4839e078a8aca2b9e21523e9c7443a34f3
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 12:14:47 -0600

    added index to email column of users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130621171823) do
+ActiveRecord::Schema.define(:version => 20130621181316) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -37,5 +37,7 @@ ActiveRecord::Schema.define(:version => 20130621171823) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
