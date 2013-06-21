c4ccb2e7633e02d80786e4a1bd47868c14535c1f
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 12:17:18 -0600

    added callback to downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first, :last
 
+  before_save { |user| user.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :first, presence: true, length: { maximum: 30 }
