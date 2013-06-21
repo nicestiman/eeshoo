8d07e38adbea5b4899c765a534cc6810b4c36589
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 17:02:26 -0600

    corrected relation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first, :last, :password, :password_confirmation
 
   has_many :assignments
-  has_many :groups, :through => :assignment
+  has_many :groups, :through => :assignments
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
