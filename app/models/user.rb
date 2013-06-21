d1b8e7dd131755f2c4906a77eae5780ffc754b6e
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 12:12:49 -0600

    implemented uniqueness to email attribute of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first, :last
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :first, presence: true, length: { maximum: 30 }
   validates :last, presence: true, length: { maximum: 30 }
 end
