b9472099a717764206524760e33b0c402e98bb52
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 11:56:24 -0600

    added email regex to validate format in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first, :last
 
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :first, presence: true, length: { maximum: 30 }
   validates :last, presence: true, length: { maximum: 30 }
 end
