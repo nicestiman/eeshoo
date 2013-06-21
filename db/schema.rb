92e3d6056a9494f74c12790f6db46e0f21e26833
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 12:22:59 -0600

    added migration for password digest column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130621181316) do
+ActiveRecord::Schema.define(:version => 20130621182057) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(:version => 20130621181316) do
     t.string   "first"
     t.string   "last"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
