24c2d7f28b89562c39488869c1a57dc871a87c8c
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 11:48:41 -0600

    added tests for email format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -48,4 +48,26 @@ describe User do
     before { @user.last = "a" * 31 }
     it { should_not be_valid }
   end
+
+  describe "when email format is invalid" do
+    it "should not be valid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
 end
