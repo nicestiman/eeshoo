54b7271f77fee045fd588284c5bed46025bdefed
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-21 12:21:18 -0600

    added test for digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,7 @@ describe User do
   it { should respond_to(:first)  }
   it { should respond_to(:last)   }
   it { should respond_to(:email)  }
+  it { should respond_to(:password_digest) }
 
   it { should be_valid }
 
