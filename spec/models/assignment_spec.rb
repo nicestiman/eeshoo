6940242d503f1962fef861f15251005b7cb1491f
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-25 11:13:38 -0600

    added test for assignment model

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -13,5 +13,14 @@
 require 'spec_helper'
 
 describe Assignment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @user = User.create(first: "Jane", last: "Doe", email: "test2@example.com", password: "testPass", password_confirmation: "testPass")
+    @group = @user.groups.create(name: "Fake Group", location: "Phoenix, Arizona, USA")
+  end
+
+  subject { @user.assignments.find_by_group_id(@group.id) }
+
+  it { should be_valid }
+  it { should respond_to(:user) }
+  it { should respond_to(:group) }
 end
