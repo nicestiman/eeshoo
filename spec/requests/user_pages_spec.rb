065d38612767bff535e12207be5287ac8f84ac14
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-26 12:48:51 -0600

    added test for new user sign up page

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "User Pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1', text: 'Sign up') }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "First",        with: "Jane"
+        fill_in "Last",         with: "Doe"
+        fill_in "Email",        with: "test@example.com"
+        fill_in "Password",     with: "testPass"
+        fill_in "Confirmation", with: "testPass"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+end
