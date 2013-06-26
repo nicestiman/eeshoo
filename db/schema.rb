626b9ccf5fe7f0bd6452ff7d37822a3108deb1dc
Bernardo Trindade <btrindade@eeshoo.com>
2013-06-26 13:59:06 -0600

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130621211341) do
+ActiveRecord::Schema.define(:version => 20130624185345) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "group_id"
