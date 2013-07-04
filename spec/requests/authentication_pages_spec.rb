e452f26fd3151eadaae4db49468f3995638d7aeb
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-03 20:27:04 -0600

    added login tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,21 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('h1',     text: 'Sign in') }
-    it { should have_selector('title',  text: 'Sign in') }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title',  text: 'Sign in') }
+      it { should have_content(text: 'Invalid') }
+
+      describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { sign_in user }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_link('Sign out', href: signout_path) }
+        
+        it { should_not have_link('Sign in', href: signin_path) }
+      end
+    end
   end
 end
