ec1d6572aea98951938f1a1654b41c97c3329aac
duncan fedde <duncan@fedde.us>
2013-07-08 11:16:05 -0600

    updates the tests

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -4,10 +4,11 @@ describe "Home pages" do
   
   subject {page}
   
-  describe "content" do
+  describe "content", js: true do
     before {visit root_path}
     it{should have_content "HIWIPI"}
     it{should have_selector 'div', id: 'globe'}
     it{should have_selector 'div', id: 'story'}
+    it{should have_selector 'svg'}
   end
 end
