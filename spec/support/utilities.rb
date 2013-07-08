5bf3b857e7a6b6068338e6cb255041d9fcf936a8
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-08 13:31:07 -0600

    edited helper sign in function for tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -3,4 +3,6 @@ def sign_in(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
+  # Sign in when not using Capybara
+  cookies[:remember_token] = user.remember_token
 end
