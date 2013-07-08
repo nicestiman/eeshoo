234e4ed434cfc0d0eedaaf88a870ba618cd14788
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-08 15:40:17 -0600

    added endpoint to list users that belong to a group

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ HIWIPI::Application.routes.draw do
     resources :posts, :only => :create,  as: "make_post" 
   end
 
+  match '/groups/:id/users', to: 'groups#user_index', via: :get, as: "members"
+
   #match '/posts' => 'posts#create'
 
   # The priority is based upon order of creation:
