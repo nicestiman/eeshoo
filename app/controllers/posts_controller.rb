d2a18a51042d50167a6528214f4fe7cdcbc84a5a
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-11 17:44:36 -0600

    fixed errors in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_filter :signed_in_user, only: [:new, :create]
+  before_filter :user_member_of_group, only: [:new, :create]
+
   def new
     @post = Post.new
     respond_to do |format|
@@ -25,7 +28,8 @@ class PostsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     @post = @group.posts.new(params[:post])
-
+    @post.author = current_user
+    
     if @post.save
       redirect_to @group 
     else
@@ -36,4 +40,20 @@ class PostsController < ApplicationController
   def tiered
     @posts = Post.all
   end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def user_member_of_group
+      @group = Group.find(params[:group_id])
+      unless current_user?(user)
+        store_location
+        redirect_to members_path(@group.id)
+      end
+    end
 end
