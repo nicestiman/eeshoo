cb62e12c4c968b7648ca1175fba932f8facef6a2
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-11 16:06:43 -0600

    added user relation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
   belongs_to :group
+  belongs_to :author, class_name: "User"
 
   validates :content, presence: true
   validates :title,   presence: true
