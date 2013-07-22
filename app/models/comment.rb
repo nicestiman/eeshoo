39c135c51462503537e38919a66ca95f3b5b0e60
duncan fedde <duncan@fedde.us>
2013-07-22 11:35:26 -0600

    adds anotations in comments to all files and relationships Relationships 	author 	post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  message    :string(255)
+#  post_id    :integer
+#  author_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :message
+  belongs_to :post
+  belongs_to :auther, class_name: "User"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130711222145) do
+ActiveRecord::Schema.define(:version => 20130722171536) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "group_id"
     t.string   "role"
     t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.string   "message"
+    t.integer  "post_id"
+    t.integer  "author_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
