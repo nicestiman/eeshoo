8bbfc50aefdff800608d7ce14410dc46db5c2962
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-23 12:03:30 -0600

    required poltergeist gem for js testing and set to default js driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
