4ca35fd577bc127880545e672997625a19756186
duncan fedde <duncan@fedde.us>
2013-07-30 15:53:30 -0600

    add the regester method to test utilitys
    regester makes a new user via the frount end based on a unsaved user
    object

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -15,3 +15,13 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def register(new_user)
+  visit signup_path
+  fill_in      "First",        with: new_user.first
+  fill_in      "Last",         with: new_user.last
+  fill_in      "Email",        with: new_user.email
+  fill_in      "Password",     with: new_user.password
+  fill_in      "Confirmation", with: new_user.password
+  click_button "Create my account"
+end
