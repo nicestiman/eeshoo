0c89310814026acd2d74466460d8e05808f8abc3
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-30 16:18:13 -0600

    added tests to translation model

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -11,5 +11,53 @@
 require 'spec_helper'
 
 describe Translation do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @trans = Translation.new(language: "en", reference: "greeting", translation: "hello world") }
+
+  subject { @trans }
+
+  it { should respond_to(:language) }
+  it { should respond_to(:refrence) }
+  it { should respond_to(:translation) }
+
+  it { should be_valid }
+
+  #tests for presence
+  describe "when language is not defined" do
+    before { @trans.language = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when reference key is not defined" do
+    before { @trans.reference = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when translation is not provided" do
+    before { @trans.translation = " " }
+    it { should_not be_valid }
+  end
+
+  #tests for format
+  describe "when language format is invalid" do
+    it "should not be valid" do
+      languages = %w[eM en. En. en-gb en-GB en- hjasdvjfnjnjklusfhusfhsdfhj]
+
+      languages.each do |invalid_lang|
+        @trans.language = invalid_lang
+        @trans.should_not be_valid
+      end
+    end
+  end
+
+  describe "when language format is valid" do
+    it "should be valid" do
+      languages = %w[en pt ch]
+
+      languages.each do |valid_lang|
+        @trans.language = valid_lang
+        @trans.should be_valid
+      end
+    end
+  end
 end
