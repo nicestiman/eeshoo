2fbf3c2b5e0b8d439cd987566d4e51ca85e75126
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-01 17:10:46 -0600

    added basic test for translation

--- spec/features/I18n_translation_spec.rb
+++ spec/features/I18n_translation_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 feature "I want to change the language" do
+  before do
+    #make a basic translation message
+    @trans = FactoryGirl.create(:translation)
+  end
+
+  let(:language_prefs) { "en-US,en;q=0.8,pt-BR;q=0.6,pt;q=0.4" }
+
+  subject { page }
+
+  context "if I am using the query string" do
+    before { visit root_path + "?locale=en" }
+
+    it "I should see the greeting in english" do
+      page.should have_content(@trans.translation)
+    end
+  end
 end
