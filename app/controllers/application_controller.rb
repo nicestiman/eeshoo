6304ea4d6db395f5b026c562541dd3cb368ca4a6
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-01 16:28:32 -0600

    implemented I18n into controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,17 @@ class ApplicationController < ActionController::Base
     super
   end
 
-  def set_locale
-    I18n.locale = params[:locale] || extract_locale_from_accept_lang_header || I18n.default_locale
-  end
-
   private
+    def set_locale
+      I18n.locale = params[:locale] || extract_locale_from_accept_lang_header || I18n.default_locale
+    end
+
     def extract_locale_from_accept_lang_header
-      available = Translation.lang_list
-      request.compatible_language_from(available)
+      available = Translation.lang_list 
+      unless available.nil? || available.empty?
+        request.compatible_language_from(available)
+      else
+        return false
+      end
     end
 end
