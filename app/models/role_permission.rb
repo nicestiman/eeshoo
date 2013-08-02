09e3253e0de9f770b7dad0a9daf943afe4bbac44
duncan fedde <duncan@fedde.us>
2013-08-02 15:39:07 -0600

    add the assosation between role and permissions
    a role has many permissions

--- app/models/role_permission.rb
+++ app/models/role_permission.rb
@@ -1,3 +1,5 @@
 class RolePermission < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :key
+
+  belongs_to :role
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Role do
   it {should respond_to(:name)}
+  it {should respond_to(:permissions)}
 end
