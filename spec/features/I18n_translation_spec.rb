261d8c9ef58e8dc8542d0c670826cb0892312a4c
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-05 16:22:35 -0600

    played around with test trying to get to stay green. Tests run green when run on their own, but red when run with all the others

--- spec/features/I18n_translation_spec.rb
+++ spec/features/I18n_translation_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 feature "I want to change the language" do
-  before do
-    #@english_trans = Translation.create(locale: "en", key: "sign_in", value: "Sign in")
+  before(:all) do
     @english_trans = FactoryGirl.create(:translation)
     @portuguese_trans = FactoryGirl.create(:translation, locale: "pt", value: "Enscrever")
   end
-  #let(:language_prefs) { "en-US,en;q=0.8,pt-BR;q=0.6,pt;q=0.4" }
-  #let(:english_trans) { FactoryGirl.create(:translation) }
-  #let(:portuguese_trans) { FactoryGirl.create(:translation, locale: "pt", value: "Enscrever") }
 
-  subject { page }
+  after(:all) do
+    Translation.delete_all
+  end
 
   context "if I am using the query string for english" do
     before do
@@ -18,6 +16,7 @@ feature "I want to change the language" do
     end
 
     it "I should see the greeting in english" do
+      save_and_open_page
       page.should have_selector("h1", text: @english_trans.value)
       page.should_not have_content("translation missing")
     end
@@ -34,9 +33,8 @@ feature "I want to change the language" do
     end
   end
 
-  context "if I am not using anything", js: true do
+  context "if I am not using anything" do
    before do
-     page.driver.headers = { 'ACCEPT-LANGUAGE' => '' }
      visit signin_path
    end
 
