8232521a1b2748aee34425fd85059b90ed4cac6d
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-05 11:01:47 -0600

    added tests that use http headers and tests for unsupported languages

--- spec/features/I18n_translation_spec.rb
+++ spec/features/I18n_translation_spec.rb
@@ -30,4 +30,45 @@ feature "I want to change the language" do
       end
     end
   end
+
+  context "if I am not using anything", js: true do
+   before do
+     @trans = FactoryGirl.create(:translation)
+     page.driver.headers = { 'ACCEPT-LANGUAGE' => '' }
+     visit signin_path
+   end
+
+   it "I should see the greeting in english" do
+     page.should_not have_content("translation missing")
+     page.should have_selector("h1", text: @trans.value)
+   end
+  end
+
+  context "if I am using a language that is not supported" do
+    context "via the query string" do
+      before do
+        @trans = FactoryGirl.create(:translation)
+        visit signin_path + "?locale=wk"
+      end
+
+      it "I should see the greeting in the default language" do
+        save_and_open_page
+        page.should_not have_content("translation missing")
+        page.should have_selector("h1", text: @trans.value)
+      end
+    end
+
+    context "via a http header", js: true do
+      before do
+        @trans = FactoryGirl.create(:translation)
+        page.driver.headers = { 'ACCEPT-LANGUAGE' => 'wk' }
+      end
+
+      it "I should see the greeting in the default language" do
+        save_and_open_page
+        page.should_not have_content("translation missing")
+        page.should have_selector("h1", text: @trans.value)
+      end
+    end
+  end
 end
