dd6518a206b0edc392d596542b78587afb162ec9
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-07 12:49:56 -0600

    added tests for portuguese

--- spec/features/I18n_translation_spec.rb
+++ spec/features/I18n_translation_spec.rb
@@ -54,4 +54,29 @@ feature "I want to change the language" do
       end
     end
   end
+
+  context "if I am using a query string for portuguese" do
+    before do
+      visit groups_path
+      visit signin_path + "?locale=pt"
+    end
+
+    scenario "I should see the greeting in portuguese" do
+      page.should_not have_content("translation missing")
+      page.should have_selector("h1", text: portuguese)
+    end
+  end
+
+  context "if I am using a http header for portuguese", js: true do
+    before do
+      visit groups_path
+      page.driver.headers = { 'ACCEPT-LANGUAGE' => 'pt' }
+      visit signin_path
+    end
+
+    scenario "I should see the greeting in portuguese" do
+      page.should_not have_content("translation missing")
+      page.should have_selector("h1", text: portuguese)
+    end
+  end
 end
