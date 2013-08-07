aa6a1dda24b1e56f6c7ac0056e78961061a359fa
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-07 11:45:57 -0600

    removed commented out section that changed I18n backend to active record

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,12 +1,2 @@
-#require 'i18n/backend/active_record'
-#I18n.backend = I18n::Backend::ActiveRecord.new
-#
-#I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
-#I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
-#I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
-#I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
-#
-#I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n.backend)
-
 REDIS_STORE = Redis.new
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(REDIS_STORE), I18n.backend)
