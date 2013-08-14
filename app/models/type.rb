dcfb4397d0497a5531a0624795b098ae5495d36f
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-14 12:43:15 -0600

    made post type object

---
+++ app/models/type.rb
@@ -0,0 +1,14 @@
+class Type
+  def self.all
+    @filenames = []
+
+    Dir.entries("./config/post_types/").each do |filename|
+      next if filename == "."
+      next if filename == ".."
+
+      @filenames.push filename.chomp(".yml")
+    end
+
+    return @filenames.uniq
+  end
+end
