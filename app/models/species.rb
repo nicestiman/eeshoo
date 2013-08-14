47489a783b2c7eaca83024408be41ed71e5f6313
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-14 15:16:14 -0600

    added initialize method to species model

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,4 +1,6 @@
 class Species
+  attr_reader :details
+
   def self.all
     @filenames = []
 
@@ -10,5 +12,11 @@ class Species
     end
 
     return @filenames.uniq
+  end
+
+  def initialize(type)
+    if all.include?(type)
+      @details = YAML.load_file("#{Rails.root}/config/post_types/#{type}.yml")
+    end
   end
 end
