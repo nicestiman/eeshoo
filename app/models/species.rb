3d7f9d73bb0a2da911c3969894bc2da58004ab9d
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-15 13:23:52 -0600

    corrected method functions

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,22 +1,29 @@
 class Species
-  attr_reader :details
+  attr_reader :details, :type
 
-  def self.all
+  def self.all(options = {})
     @filenames = []
 
     Dir.entries("./config/post_types/").each do |filename|
       next if filename == "."
       next if filename == ".."
 
-      @filenames.push [filename.chomp(".yml"), filename.chomp(".yml")]
+      if options[:selectable] == true
+        @filenames.push [filename.chomp(".yml"), filename.chomp(".yml")]
+      else
+        @filenames.push filename.chomp(".yml")
+      end
     end
 
     return @filenames.uniq
   end
 
   def initialize(type)
-    if Species.all.include?(type)
-      @details = YAML.load_file("#{Rails.root}/config/post_types/#{type}.yml")
+    @type = type.to_s
+    if Species.all.include?(@type)
+      @details = YAML.load_file("#{Rails.root}/config/post_types/#{@type}.yml")
+    else
+      return false
     end
   end
 end
