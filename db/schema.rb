a8fc5c85e7399e4cd000639da24f0b307f0ca7f1
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-15 15:01:59 -0600

    removed title from post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130809214636) do
+ActiveRecord::Schema.define(:version => 20130815205336) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "group_id"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(:version => 20130809214636) do
 
   create_table "posts", :force => true do |t|
     t.text     "content"
-    t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "group_id"
