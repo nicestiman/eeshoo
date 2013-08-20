e2898d5e21c279f27e63f1977fee32ccc81f230d
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-20 10:52:36 -0600

    removed title from posts made in groups specs

--- spec/requests/group_pages_spec.rb
+++ spec/requests/group_pages_spec.rb
@@ -80,8 +80,8 @@ describe "Group pages" do
   describe "group profile page with posts" do
     let(:author) { FactoryGirl.create(:user) }
     before do
-      @post1 = @group.posts.new(content: "this is a test post", title: "Test post", species: "default")
-      @post2 = @group.posts.new(content: "this is another test post", title: "Second Test Post", species: "default")
+      @post1 = @group.posts.new(content: "this is a test post", species: "default")
+      @post2 = @group.posts.new(content: "this is another test post", species: "default")
       @posts = [@post1, @post2]
       @posts.each do |post|
         post.author = author
@@ -95,7 +95,6 @@ describe "Group pages" do
     
     it "should list each post" do
       @group.posts.each do |post|
-        page.should     have_selector("li", text: post.title)
         page.should     have_selector("a",  href: group_post_path(@group.id, post.id))
         page.should_not have_content(post.content)
       end
